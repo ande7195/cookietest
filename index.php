9d1b2493ddcbca6f45c4882c88a63ba4c9d9cc2d
ande7195 <ande7195@edu.easj.dk>
2017-10-04 11:26:31 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AKCGa
+ * Date: 04-10-2017
+ * Time: 11:00
+ */
+$cookie_name= "user";
+$cookie_value= "Alex Porter";
+setcookie($cookie_name,$cookie_value,time()+(86400*30),"/");
+
+
+?>
+<html>
+<body>
+<?php
+if(!isset($_COOKIE[$cookie_name])){
+    echo "Cookie Named '".$cookie_name." ' is not set!";
+}else{
+    echo "Cookie '".$cookie_name."' is set! <br>";
+    echo "Value is: ".$_COOKIE[$cookie_name];
+}
+?>
+
+</body>
+</html>
+<?php
+// set the expiration date to one hour ago
+setcookie("user", "", time() - 3600);
+?>
+<html>
+<body>
+
+<?php
+echo "<br>"."Cookie 'user' is deleted.";
+?>
+
+</body>
+</html>
